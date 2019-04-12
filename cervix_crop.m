a10117d6d5c533e085836b6c142a658f3e005c78
Mercy Asiedu <mercy.asiedu@duke.edu>
2019-04-11 23:17:39 -0400

    description and new folder

--- cervix_crop.m
+++ cervix_crop.m
@@ -1,14 +1,19 @@
+%Code to perform cropping at scale. Use to crop cervix images to remove clinically irrelevant
+%data such as the speculum and vaginal walls
+%Writtin by Mercy Nyamewaa Asiedu
+%Last updated 04/11/2019
 close all; clear all;
-cd('X:\Mercy\Image processing\VIA image processing\Honduras');
+cd('X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Zambia\White Images');
 dirlist = [dir('*.tif');dir('*.jpg');dir('*.png')];
 cd('X:\Mercy\Image processing\Segmentation');
-folder='X:\Mercy\Image processing\VIA image processing\Honduras';
-for n=50:51 %length(dirlist)
+folder='X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Zambia\White Images';
+for n=50:50%length(dirlist)
     %read in data
-    name=['W',num2str(n),'.jpg'];
+    name=['ZG',num2str(n),'.tif'];
     fullfilename=fullfile(folder, name);
     image=imread(fullfilename);
     cervix_boxcrop=imcrop(image);
-    filename=['X:\Mercy\Image processing\Segmentation\cervix_boxcrop\L',num2str(n),'.jpg'];
+    filename=['X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Zambia\White Images\cervix_boxcrop\ZG',num2str(n),'.tif'];
     imwrite(cervix_boxcrop,filename);  
 end
+cd('X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Zambia\White Images');
