d11d63a0cac3e0ac5d759554201e8b49efe458c6
Mercy Asiedu <mercy.asiedu@duke.edu>
2019-04-12 11:30:44 -0400

    Add files via upload

--- cervix_crop.m
+++ cervix_crop.m
@@ -1,19 +1,19 @@
 %Code to perform cropping at scale. Use to crop cervix images to remove clinically irrelevant
 %data such as the speculum and vaginal walls
-%Writtin by Mercy Nyamewaa Asiedu
+%Written by Mercy N. Asiedu
 %Last updated 04/11/2019
 close all; clear all;
-cd('X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Zambia\White Images');
+cd('X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Mombasa_Ganjoni\AA White Light');
 dirlist = [dir('*.tif');dir('*.jpg');dir('*.png')];
 cd('X:\Mercy\Image processing\Segmentation');
-folder='X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Zambia\White Images';
-for n=50:50%length(dirlist)
+folder='X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Mombasa_Ganjoni\AA White Light';
+for n=270:361%length(dirlist)
     %read in data
-    name=['ZG',num2str(n),'.tif'];
+    name=['GW',num2str(n),'.tif'];
     fullfilename=fullfile(folder, name);
     image=imread(fullfilename);
     cervix_boxcrop=imcrop(image);
-    filename=['X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Zambia\White Images\cervix_boxcrop\ZG',num2str(n),'.tif'];
+    filename=['X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Mombasa_Ganjoni\AA White Light\cervix_boxcrop\GW',num2str(n),'.tif'];
     imwrite(cervix_boxcrop,filename);  
 end
-cd('X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Zambia\White Images');
+cd('X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Mombasa_Ganjoni\AA White Light');
