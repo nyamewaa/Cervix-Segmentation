c80472b704f417d4038811ec113e8d2e0e2da786
Mercy Asiedu <mercy.asiedu@duke.edu>
2019-04-13 15:00:59 -0400

    processing honduras images

--- cervix_crop.m
+++ cervix_crop.m
@@ -1,19 +1,19 @@
 %Code to perform cropping at scale. Use to crop cervix images to remove clinically irrelevant
 %data such as the speculum and vaginal walls
 %Written by Mercy N. Asiedu
-%Last updated 04/11/2019
+%Last updated 04/13/2019
 close all; clear all;
-cd('X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Mombasa_Ganjoni\AA White Light');
+cd('X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Honduras\Green Light');
 dirlist = [dir('*.tif');dir('*.jpg');dir('*.png')];
-cd('X:\Mercy\Image processing\Segmentation');
-folder='X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Mombasa_Ganjoni\AA White Light';
-for n=270:361%length(dirlist)
+%cd('X:\Mercy\Image processing\Segmentation');
+folder='X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Honduras\Green Light';
+for n=1:612%length(dirlist)
     %read in data
-    name=['GW',num2str(n),'.tif'];
+    name=['HG',num2str(n),'.jpg'];
     fullfilename=fullfile(folder, name);
     image=imread(fullfilename);
     cervix_boxcrop=imcrop(image);
-    filename=['X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Mombasa_Ganjoni\AA White Light\cervix_boxcrop\GW',num2str(n),'.tif'];
+    filename=['X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Honduras\Green Light\cervix_boxcrop\HG',num2str(n),'.jpg'];
     imwrite(cervix_boxcrop,filename);  
 end
-cd('X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Mombasa_Ganjoni\AA White Light');
+cd('X:\Mercy\Image processing\VIA image processing\Processing_without_ectopion\Honduras\Green Light');
